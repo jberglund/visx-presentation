cd0f385d6826f0a30dfb89434f03d8ce4a9872c8
jberglund <jacob@jberglund.se>
2022-05-06 13:48:41 +0200

    4 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css';
-/* import Steps1 from './Steps/1';
+import Steps1 from './Steps/1';
 import Steps2 from './Steps/2';
 import Steps3 from './Steps/3';
 import Steps4 from './Steps/4';
-import Steps5 from './Steps/5'; */
+import Steps5 from './Steps/5';
 
 import Graph from './Components/Graph';
 
@@ -12,11 +12,11 @@ function App() {
     <main className="main">
       <div>Visx med Jacob!</div>
       <Graph />
-      {/* <Steps1 />
+      <Steps1 />
       <Steps2 />
       <Steps3 />
       <Steps4 />
-      <Steps5 /> */}
+      <Steps5 />
     </main>
   );
 }
--- src/Components/Graph.tsx
+++ src/Components/Graph.tsx
@@ -1,5 +1,11 @@
-import { height, width } from '../config';
-
+import { scaleLinear, scaleTime } from '@visx/scale';
+import { LinePath } from '@visx/shape';
+import { curveNatural } from '@visx/curve';
+import Circle from '@visx/shape/lib/shapes/Circle';
+import { Grid } from '@visx/grid';
+import { getMinMax } from '../helpers';
+import { width, height } from '../config';
+import { AxisBottom } from '@visx/axis';
 /*
   TODO
   + Mappa över array av nummer
@@ -14,10 +20,80 @@ import { height, width } from '../config';
 
 */
 
-export default function Graph() {
+// paths
+
+type Data = {
+  value: number;
+  date: Date;
+};
+
+const data: Data[] = [
+  { value: 160, date: new Date('2022-04-01') },
+  { value: 176, date: new Date('2022-04-02') },
+  { value: 220, date: new Date('2022-04-03') },
+  { value: 155, date: new Date('2022-04-04') },
+  { value: 321, date: new Date('2022-04-05') },
+  { value: 245, date: new Date('2022-04-06') },
+  { value: 222, date: new Date('2022-04-07') },
+];
+
+const getValue = (i: Data) => i.value;
+const getDate = (i: Data) => i.date.getTime();
+
+const paddingTop = 10;
+
+const [minX, maxX] = getMinMax(data.map(getDate));
+const [minY, maxY] = getMinMax(data.map(getValue));
+
+export default function Bars() {
+  const yScale = scaleLinear({
+    range: [paddingTop, height - paddingTop],
+    domain: [minY, maxY],
+    nice: true,
+  });
+
+  const xScale = scaleTime({
+    range: [0, width],
+    domain: [minX, maxX],
+    round: true,
+  });
+
   return (
     <svg height={height} width={width} xmlns="http://www.w3.org/2000/svg">
-      {/* <rect x="0" width="50" height="150" y="50" rx="5" /> */}
+      <Grid
+        xScale={xScale}
+        yScale={yScale}
+        width={width}
+        height={height}
+        numTicksRows={0}
+        numTicksColumns={10}
+        strokeOpacity={0.1}
+      />
+      <AxisBottom
+          top={height}
+          scale={xScale}
+          tickLabelProps={() => ({
+            fill: 'red',
+            fontSize: 11,
+            textAnchor: 'middle',
+          })}
+        />
+
+      <LinePath
+        data={data}
+        x={(d) => xScale(getDate(d)) ?? 0}
+        y={(d) => yScale(getValue(d)) ?? 0}
+        curve={curveNatural}
+        strokeWidth={2}
+      />
+      {data.map((d) => (
+        <Circle r={5} cx={xScale(getDate(d))} cy={yScale(getValue(d))} />
+      ))}
     </svg>
   );
 }
+
+/* cheat codes
+
+
+*/
